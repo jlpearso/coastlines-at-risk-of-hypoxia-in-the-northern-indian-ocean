dc70df36c2139a685c5f2cd7dc901e7b11bb0cc7
Jenna Pearson <jennalynnpearson@gmail.com>
2021-11-30 18:03:10 -0500

    updates and fix for processing code for WOD profiles

--- data_processing/0_WOD/raw_data/get_data/concat_and_process.py
+++ data_processing/0_WOD/raw_data/get_data/concat_and_process.py
@@ -1,4 +1,4 @@
-inpath = 'latest/'
+inpath = 'individual/'
 outfn = 'all_casts_beginning_to_aug_2020_indian_ocean.nc'
   
 
@@ -26,13 +26,16 @@ file.writelines('File: ' + outfn + ', Processing Date: ' + today.strftime("%m/%d
 # get all the datafiles
 fns = glob.glob(inpath + '*.nc')
 
-# can uncomment below to get full range of depth - I choose to cut it off at 2000
+# can modify below to only get data to a certain depth, just remove what you don't need.
 # see https://www.ncei.noaa.gov/access/world-ocean-database-select/depth_definition.html
 # for depth definitions 
 DEPTH = np.array([0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,
                  125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500,
                  550,600,650,700,750,800,850,900,950,1000,1050,1100,1150,1200,1250,1300,
-                 1350,1400,1450,1500])
+                 1350,1400,1450,1500,1550,1600,1650,1700,1750,1800,1850,1900,1950,2000,
+                 2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,
+                 3100,3200,3300,3400,3500,3600,3700,3800,3900,4000,
+                 4100,4200,4300,4400,4500,4600,4700,4800,4900,5000])
 # loop through to get the total number of profiles for variable initialization
 no_profs = 0
 for ff,fn in enumerate(fns):
@@ -198,6 +201,7 @@ for vv, var in enumerate(varlist):
                             ax5.plot(PHOSPHATE[pr,:],-1*DEPTH) 
 
                 pr = pr + 1
+                
                         
                 # move index up
                 if "Temperature" in ds_in:
@@ -215,6 +219,8 @@ for vv, var in enumerate(varlist):
                 if "Phosphate" in ds_in:
                     if (~np.isnan(p_sz)):
                         phosphate_row_count = phosphate_row_count + int(p_sz)
+                        
+                z_row_count = z_row_count + int(z_sz)
 
 file.close()
 ax6.scatter(lon,lat)
@@ -254,3 +260,4 @@ if os.path.isfile(outfn):
 
 ds_out.to_netcdf(outfn,mode='w',format = "NETCDF4")
 ds_out
+
